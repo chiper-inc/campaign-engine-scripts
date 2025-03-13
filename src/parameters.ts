511d02963daae042ec5823d2499738ac6010cf93
Jimmy Arismendy Sanabria <jimmy@chiper.co>
2025-03-13 16:18:33 -0500

    .

--- src/parameters.ts
+++ src/parameters.ts
@@ -1,5 +1,5 @@
-import { LOCATION, STORE_STATUS } from "./enums.ts";
-import { TypeCampaignByStatus, TypeFrequencyByStatus } from "./types.ts";
+import { LOCATION, STORE_STATUS } from './enums.ts';
+import { TypeCampaignByStatus, TypeFrequencyByStatus } from './types.ts';
 
 export const frequencyByStatus: TypeFrequencyByStatus = {
   [STORE_STATUS.Churn]: {
@@ -65,24 +65,16 @@ const generateParams = (
 ) => 
   Array.from({ length }, (_, i) => params.map(p => `${p}_${i + 1}`)).flat();
 
-const NAME = ["name"];
-const DEFAULT = NAME.concat(generateParams(["sku", "dsct"], 2));
-const SKU_DSCT = ["sku", "dsct"];
-const SKU_DSCT_IMG = ["sku", "dsct", "img"];
+const NAME = ['name'];
+const DEFAULT = NAME.concat(generateParams(['sku', 'dsct'], 2));
+const SKU_DSCT = ['sku', 'dsct'];
+const SKU_DSCT_IMG = ['sku', 'dsct', 'img'];
+
+export const TESTING_CLIENTS = ['+573153108376'];
 
 export const campaignsBySatatus: TypeCampaignByStatus = {
   [STORE_STATUS.Churn]: {
-    names: [
-      "API_Churn_1_es_v0",
-      "API_Churn_2_es_v0",
-      "API_Churn_3_es_v0",
-      "API_Churn_4_es_v0",
-      "API_Churn_5_es_v0",
-      "API_Churn_6_es_v0",
-      "API_Churn_7_es_v0",
-      "API_Churn_8_es_v0",
-      "API_Churn_9_es_v0",
-    ],
+    names: [],
     variables: {
       _default: DEFAULT,
       API_Churn_1_es_v0: NAME.concat(generateParams(SKU_DSCT, 1)),
@@ -97,17 +89,7 @@ export const campaignsBySatatus: TypeCampaignByStatus = {
     }
   },
   [STORE_STATUS.Lead]: {
-    names: [
-      "API_Lead_1_es_v0",
-      "API_Lead_2_es_v0",
-      "API_Lead_3_es_v0",
-      "API_Lead_4_es_v0",
-      "API_Lead_5_es_v0",
-      "API_Lead_6_es_v0",
-      "API_Lead_7_es_v1",
-      "API_Lead_8_es_v0",
-      "API_Lead_9_es_v0",
-    ],
+    names: [],
     variables: {
       _default: DEFAULT,
       API_Lead_1_es_v0: NAME.concat(generateParams(SKU_DSCT, 1)),
@@ -122,17 +104,7 @@ export const campaignsBySatatus: TypeCampaignByStatus = {
     }
   },
   [STORE_STATUS.New]: {
-    names: [
-      "API_New_1_es_v0",
-      "API_New_2_es_v0",
-      "API_New_3_es_v0",
-      "API_New_4_es_v0",
-      "API_New_5_es_v0",
-      "API_New_6_es_v0",
-      "API_New_7_es_v0",
-      "API_New_8_es_v1",
-      "API_New_9_es_v0",
-    ],
+    names: [],
     variables: {
       _default: DEFAULT,
       API_New_1_es_v0: NAME.concat(generateParams(SKU_DSCT, 1)),
@@ -147,17 +119,7 @@ export const campaignsBySatatus: TypeCampaignByStatus = {
     },
   },
   [STORE_STATUS.Hibernating]: {
-    names: [
-      "API_Hibernating_1_es_v0",
-      "API_Hibernating_2_es_v0",
-      "API_Hibernating_3_es_v0",
-      "API_Hibernating_4_es_v0",
-      "API_Hibernating_5_es_v0",
-      "API_Hibernating_6_es_v0",
-      "API_Hibernating_7_es_v1",
-      "API_Hibernating_8_es_v2",
-      "API_Hibernating_9_es_v0",
-    ],
+    names: [],
     variables: {
       _default: DEFAULT,
       API_Hibernating_1_es_v0: NAME.concat(generateParams(SKU_DSCT, 1)),
@@ -173,17 +135,7 @@ export const campaignsBySatatus: TypeCampaignByStatus = {
 
   },
   [STORE_STATUS.Retained]: {
-    names: [
-      "API_Retained_1_es_v0",
-      "API_Retained_2_es_v0",
-      "API_Retained_3_es_v0",
-      "API_Retained_4_es_v0",
-      "API_Retained_5_es_v0",
-      "API_Retained_6_es_v0",
-      "API_Retained_7_es_v0",
-      "API_Retained_8_es_v1",
-      "API_Retained_9_es_v1",
-    ],
+    names: [],
     variables: {
       _default: DEFAULT,
       API_Retained_1_es_v0: NAME.concat(generateParams(SKU_DSCT, 1)),
@@ -199,17 +151,7 @@ export const campaignsBySatatus: TypeCampaignByStatus = {
 
   },
   [STORE_STATUS.Resurrected]: {
-    names: [
-      "API_Resurrected_1_es_v0",
-      "API_Resurrected_2_es_v0",
-      "API_Resurrected_3_es_v0",
-      "API_Resurrected_4_es_v0",
-      "API_Resurrected_5_es_v0",
-      "API_Resurrected_6_es_v0",
-      "API_Resurrected_7_es_v1",
-      "API_Resurrected_8_es_v1",
-      "API_Resurrected_9_es_v0",
-    ],
+    names: [],
     variables: {
       _default: DEFAULT,
       API_Resurrected_1_es_v0: NAME.concat(generateParams(SKU_DSCT, 1)),
@@ -228,5 +170,16 @@ export const campaignsBySatatus: TypeCampaignByStatus = {
     variables: {
       _default: NAME,
     }
+  }
+}
+
+for (const campaignStatus in campaignsBySatatus) {
+  const status = campaignStatus as unknown as STORE_STATUS;
+  campaignsBySatatus[status].names = [];
+  for (const campaign in campaignsBySatatus[status]?.variables) {
+    if (campaign === '_default') {
+      continue;
+    }
+    campaignsBySatatus[status].names.push(campaign);
   }
 }
