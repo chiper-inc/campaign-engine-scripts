5358b870c542ffe5fe295261e1fc041175e6e14c
Jimmy Arismendy Sanabria <jimmy@chiper.co>
2025-04-28 09:11:12 -0500

    Initial Aproach

---
+++ src/providers/campaign-metadata.ts
@@ -0,0 +1,127 @@
+export interface IMetadata {
+  storeId: number;
+  campaignName: string; // Object | string;
+  recommendations: IMetadataRecommendation[];
+}
+
+export interface IMetadataRecommendation {
+  externalId: string; // Phone number | uuId-xxxxx-uuId
+  conntent: IMessageContent;
+}
+
+export interface IMetadataRecommendationConnectly
+  extends IMetadataRecommendation {
+  sendoutName: string; // Campaign Name
+  sendoutId: string; // sendoutId
+}
+
+export interface IMetadataRecommendationClevertap
+  extends IMetadataRecommendation {
+  campaingId: string; // Clevertap Campaign Id
+}
+
+export interface IMessageContent {
+  campaignContent: string; // uuid
+  storeReferenceId: number | null;
+  referencePromotionId: number | null;
+}
+
+export class CampaignMetadata implements IMetadata {
+  public storeId: number;
+  public campaignName: string;
+  public recommendations: MetadataRecommendation[];
+
+  constructor({
+    storeId,
+    campaignName,
+    recommendations,
+  }: {
+    storeId: number;
+    campaignName: string;
+    recommendations: MetadataRecommendation[];
+  }) {
+    this.storeId = storeId;
+    this.campaignName = campaignName;
+    this.recommendations = recommendations;
+  }
+}
+
+export class MetadataRecommendation implements IMetadataRecommendation {
+  public externalId: string; // Phone number | uuId-xxxxx-uuId
+  public conntent: MetadataMessageContent;
+
+  constructor({
+    externalId,
+    conntent,
+  }: {
+    externalId: string;
+    conntent: MetadataMessageContent;
+  }) {
+    this.externalId = externalId;
+    this.conntent = conntent;
+  }
+}
+
+export class MetadataRecomendationClevertap
+  extends MetadataRecommendation
+  implements IMetadataRecommendationClevertap
+{
+  public campaingId: string; // Clevertap Campaign Id
+
+  constructor({
+    externalId,
+    conntent,
+    campaingId,
+  }: {
+    externalId: string;
+    conntent: MetadataMessageContent;
+    campaingId: string;
+  }) {
+    super({ externalId, conntent });
+    this.campaingId = campaingId;
+  }
+}
+
+export class MetadataRecomendationConnectly
+  extends MetadataRecommendation
+  implements IMetadataRecommendationConnectly
+{
+  public sendoutName: string; // Campaign Name
+  public sendoutId: string; // sendoutId
+
+  constructor({
+    externalId,
+    conntent,
+    sendoutName,
+    sendoutId,
+  }: {
+    externalId: string;
+    conntent: MetadataMessageContent;
+    sendoutName: string;
+    sendoutId: string;
+  }) {
+    super({ externalId, conntent });
+    this.sendoutName = sendoutName;
+    this.sendoutId = sendoutId;
+  }
+}
+
+export class MetadataMessageContent implements IMessageContent {
+  public campaignContent: string; // uuid
+  public storeReferenceId: number | null;
+  public referencePromotionId: number | null;
+
+  constructor({
+    campaignContent,
+    storeReferenceId,
+    referencePromotionId,
+  }: {
+    campaignContent: string;
+    storeReferenceId: number | null;
+    referencePromotionId: number | null;
+  }) {
+    this.campaignContent = campaignContent;
+    this.storeReferenceId = storeReferenceId;
+    this.referencePromotionId = referencePromotionId;
+  }
+}
